0061cd0aa4e2014a3d7482643486585a008be6ac
Snekapritha <snehapritha@gmail.com>
2016-08-10 17:58:30 +0530

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,10 @@
+import.java.util.*;
+ 
+ public class Length{
+   public static void main(String args[]){
+   String str="";
+   Scanner s=new Scanner(System.in);
+   str=s.next();
+   System.out.println("The length of the string is"+str.length());
+   }
+   }
